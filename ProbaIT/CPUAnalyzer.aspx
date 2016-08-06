2a8eb531fb73071189dbc27d0e8b337730116274
Emil95 <e.nikolovski95@gmail.com>
2016-08-06 13:42:12 +0200

    Created web service

---
+++ ProbaIT/CPUAnalyzer.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Default.Master" AutoEventWireup="true" CodeBehind="CPUAnalyzer.aspx.cs" Inherits="ProbaIT.Default1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:GridView ID="items" runat="server"></asp:GridView>
+
+    </asp:Content>
---
+++ ProbaIT/CPUAnalyzer.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ProbaIT
+{
+    public partial class CPUAnalyzer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = ConfigurationManager.ConnectionStrings["ITProekt"].ConnectionString;
+            String querry = "";
+            SqlCommand command = new SqlCommand(querry,connection);
+            
+        }
+    }
+}
