bb4b1cf33e549445cbf4f104dd13ff3e962a7948
Emil95 <e.nikolovski95@gmail.com>
2016-08-17 02:04:27 +0200

    Check CPU Performance (Web Service ) Done.

--- ProbaIT/CPUPerformance.asmx.cs
+++ ProbaIT/CPUPerformance.asmx.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace ProbaIT
 {
@@ -17,10 +20,43 @@ namespace ProbaIT
     public class CPUPerformance : System.Web.Services.WebService
     {
 
-        [WebMethod]
-        public string HelloWorld()
+        [WebMethod(Description ="This is web service for comparing the performances of two CPUs passed by ID number as arguments to the function")]
+        public DataSet  Compare(string ID1, string ID2)
         {
-            return "Hello World";
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = ConfigurationManager.ConnectionStrings["ITProekt"].ConnectionString;
+            String querry = "SELECT Name , Cores , Threads , Clock , Cache , Score  FROM Processors WHERE Id=@P1 OR Id= @P2";
+           
+            SqlCommand command = new SqlCommand(querry, connection);
+            command.Parameters.AddWithValue("@P1", ID1);
+            command.Parameters.AddWithValue("@P2", ID2);
+            List<String> list = new List<String>();
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+            adapter.SelectCommand = command;
+            try
+            {
+                connection.Open();
+               
+                adapter.Fill(ds, "CPU");
+            
+            }
+            catch (Exception err)
+            {
+                //  errMsg.Text= err.Message;
+
+
+            }
+            finally
+            {
+                connection.Close();
+
+            }
+
+            
+            return ds;
+           
         }
     }
 }
