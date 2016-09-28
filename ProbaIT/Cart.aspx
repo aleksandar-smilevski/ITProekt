489f9429d3f6a73f24cd5ccf34e98b28aa9a2027
Viktor <viktorsiljnovski21@gmail.com>
2016-09-28 03:55:47 +0200

    Created cart page

---
+++ ProbaIT/Cart.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Default.Master" AutoEventWireup="true" CodeBehind="Cart.aspx.cs" Inherits="ProbaIT.Cart" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style type="text/css">
+        .auto-style1 {
+            background-color: rgba(0, 0, 0, 0.7);
+            margin-top: 15px;
+            margin-bottom: 10px;
+            padding: 12px 24px;
+            color: white;
+            border-radius: 4px;
+            margin-left: 40px;
+        }
+    </style>
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <div class="col-sm-9 col-lg-10 backImg" style="height: 100vh">
+        <div id="divCart" class="row well-grey text-center" style="margin-left: 30%; margin-top: 20%; width: 363px; height: 60px;" runat="server">
+            <asp:Label runat="server" Text="Your cart is empty!" Font-Bold="True" ID="lblCartEmpty" Font-Size="20pt" ForeColor="White" CssClass="text-center"></asp:Label>
+        </div>
+        <div id="divGrid" runat="server">
+            <div class="text-center" style="margin-left: 30%; margin-bottom: 10%; margin-top: 20%;">
+                <asp:GridView ID="gvCart" runat="server" BackColor="White" BorderColor="#999999" BorderStyle="None" BorderWidth="1px" CellPadding="3" GridLines="Vertical" AutoGenerateColumns="False" CssClass="auto-style2" Width="567px" AllowPaging="True" OnPageIndexChanging="gvCart_PageIndexChanging" OnRowCancelingEdit="gvCart_RowCancelingEdit" OnRowEditing="gvCart_RowEditing" OnRowUpdating="gvCart_RowUpdating" PageSize="5" OnRowDeleting="gvCart_RowDeleting">
+                    <AlternatingRowStyle BackColor="Gainsboro" />
+                    <Columns>
+                        <asp:ImageField ReadOnly="True">
+                        </asp:ImageField>
+                        <asp:BoundField DataField="Type" HeaderText="Type" ReadOnly="True" />
+                        <asp:BoundField DataField="Name" HeaderText="Name of product" ReadOnly="True" />
+                        <asp:BoundField DataField="Description" HeaderText="Description" ReadOnly="True" />
+                        <asp:BoundField DataField="Price" HeaderText="Price per unit" ReadOnly="True" />
+                        <asp:BoundField HeaderText="Quantity" DataField="Quantity" />
+                        <asp:CommandField ShowEditButton="True" ShowDeleteButton="True" />
+                    </Columns>
+                    <FooterStyle BackColor="#CCCCCC" ForeColor="Black" />
+                    <HeaderStyle BackColor="#000084" Font-Bold="True" ForeColor="White" />
+                    <PagerStyle BackColor="#999999" ForeColor="Black" HorizontalAlign="Center" />
+                    <RowStyle BackColor="#EEEEEE" ForeColor="Black" />
+                    <SelectedRowStyle BackColor="#008A8C" Font-Bold="True" ForeColor="White" />
+                    <SortedAscendingCellStyle BackColor="#F1F1F1" />
+                    <SortedAscendingHeaderStyle BackColor="#0000A9" />
+                    <SortedDescendingCellStyle BackColor="#CAC9C9" />
+                    <SortedDescendingHeaderStyle BackColor="#000065" />
+                </asp:GridView>
+                &nbsp;&nbsp;&nbsp;
+                <div class="auto-style1" style="width: 432px">
+                    <asp:Label ID="lblPriceInCart" runat="server" Text="Price in cart:" ForeColor="White" Font-Size="14pt"></asp:Label>
+                    &nbsp;&nbsp;&nbsp;
+                    <asp:TextBox ID="tbPrice" runat="server" ReadOnly="True" Enabled="False" BackColor="White" ForeColor="Black" Width="75px">0</asp:TextBox>
+                </div>
+                
+            </div>
+        </div>
+</asp:Content>
---
+++ ProbaIT/Cart.aspx.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Diagnostics;
+
+namespace ProbaIT
+{
+    public partial class Cart : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Session["cart"] != null)
+                {
+                    divCart.Style.Add("display", "none");
+                    divGrid.Style.Add("display", "block");
+                    updateGrid();
+                }
+                else
+                {
+                    divCart.Style.Add("display", "block");
+                    gvCart.Visible = false;
+                }
+            }
+        }
+
+        private void updateGrid()
+        {
+            ViewState["dataTable"] = null;
+            DataTable dt = new DataTable();
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = ConfigurationManager.ConnectionStrings["ITProekt"].ConnectionString;
+            string query;
+            StringBuilder description;
+            foreach (Product p in (HashSet<Product>)Session["cart"])
+            {
+                if (ViewState["dataTable"] != null)
+                {
+                    dt = (DataTable)ViewState["dataTable"];
+                }
+                else
+                {
+                    dt.Columns.AddRange(new DataColumn[5] { new DataColumn("Type"), new DataColumn("Name"), new DataColumn("Description"), new DataColumn("Price"), new DataColumn("Quantity") });
+                }
+
+                if (p.ProductType.Equals("Processor"))
+                {
+                    query = "SELECT Name," + "Cores, Threads, Clock, Cache, Price FROM Processors WHERE Name='" + p.ModelName + "'";
+
+                }
+                else if (p.ProductType.Equals("MotherBoard"))
+                {
+                    query = "SELECT Name, Description, Price FROM Motherboards WHERE Name='" + p.ModelName + "'";
+                }
+                else if (p.ProductType.Equals("PowerUnit"))
+                {
+                    query = "SELECT Name, Description, Price FROM PowerUnits WHERE Name='" + p.ModelName + "'";
+                }
+                else if (p.ProductType.Equals("GraphicsCard"))
+                {
+                    query = "SELECT Name, Description, Price FROM GrapicsCards WHERE Name='" + p.ModelName + "'";
+                }
+                else if (p.ProductType.Equals("RAM"))
+                {
+                    query = "SELECT Name, Description, Price FROM RAMS WHERE Name='" + p.ModelName + "'";
+                }
+                else
+                {
+                    query = "SELECT Name, Description, Price FROM HDDS WHERE Name='" + p.ModelName + "'";
+                }
+
+                SqlCommand command = new SqlCommand(query, connection);
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        if (p.ProductType.Equals("Processor"))
+                        {
+                            description = new StringBuilder();
+                            description.Append(string.Format("Cores: {0}; Threads: {1}; Clock: {2}; Cache: {3};", reader["Cores"].ToString(), reader["Threads"].ToString(), reader["Clock"].ToString(), reader["Cache"].ToString()));
+                            dt.Rows.Add(p.ProductType, reader["Name"].ToString(), description.ToString(), reader["Price"], p.Quantity.ToString());
+                        }
+                        else
+                        {
+                            dt.Rows.Add(p.ProductType, reader["Name"].ToString(), reader["Description"].ToString(), reader["Price"], p.Quantity.ToString());
+                        }
+                        int price = int.Parse(tbPrice.Text);
+                        tbPrice.Text = (price + (int.Parse(reader["Price"].ToString()) * p.Quantity)).ToString();
+                        gvCart.DataSource = dt;
+                        gvCart.DataBind();
+                        ViewState["dataTable"] = dt;
+                    }
+                }
+                catch (Exception err)
+                {
+                    Debug.Write(err.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        protected void gvCart_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            DataTable dt = (DataTable)ViewState["dataTable"];
+            gvCart.EditIndex = e.NewEditIndex;
+            gvCart.DataSource = dt;
+            gvCart.DataBind();
+        }
+
+        protected void gvCart_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            DataTable dt = (DataTable)ViewState["dataTable"];
+            gvCart.EditIndex = -1;
+            gvCart.DataSource = dt;
+            gvCart.DataBind();
+        }
+
+        protected void gvCart_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            gvCart.PageIndex = e.NewPageIndex;
+            gvCart.EditIndex = -1;
+            DataTable set = (DataTable)ViewState["dataTable"];
+            gvCart.DataSource = set;
+            gvCart.DataBind();
+        }
+
+        protected void gvCart_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            TextBox tb = (TextBox)gvCart.Rows[e.RowIndex].Cells[5].Controls[0];
+            Product p = new Product(gvCart.Rows[e.RowIndex].Cells[1].Text, gvCart.Rows[e.RowIndex].Cells[2].Text, int.Parse(tb.Text));
+            HashSet<Product> set = (HashSet<Product>)Session["cart"];
+            set.Remove(p);
+            set.Add(p);
+            Session["cart"] = set;
+            gvCart.EditIndex = -1;
+            tbPrice.Text = "0";
+            updateGrid();
+        }
+
+        protected void gvCart_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            Product p = new Product(gvCart.Rows[e.RowIndex].Cells[1].Text, gvCart.Rows[e.RowIndex].Cells[2].Text, int.Parse(gvCart.Rows[e.RowIndex].Cells[5].Text));
+            HashSet<Product> set = (HashSet<Product>)Session["cart"];
+            set.Remove(p);
+            Session["cart"] = set;
+            tbPrice.Text = "0";
+            if (set.Count == 0)
+            {
+                divGrid.Style.Add("display", "none");
+                divCart.Style.Add("display", "block");
+            }
+            else
+                updateGrid();
+        }
+    }
+}
