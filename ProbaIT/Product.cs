2debfe8b165ec479f699e700887f0c3f71a3a378
Viktor <viktorsiljnovski21@gmail.com>
2016-09-28 03:59:59 +0200

    Made product class

---
+++ ProbaIT/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProbaIT
+{
+    public class Product
+    {
+        public string ProductType { get; set; }
+        public string ModelName { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string productType, string modelName, int quantity)
+        {
+            ProductType = productType;
+            ModelName = modelName;
+            Quantity = quantity;
+        }
+
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+            Product product = obj as Product;
+
+            if (obj == null)
+            {
+                return false;
+            }
+
+            return ModelName.Equals(product.ModelName);
+        }
+
+        public override int GetHashCode()
+        {
+            return ModelName.GetHashCode();
+        }
+    }
+}
